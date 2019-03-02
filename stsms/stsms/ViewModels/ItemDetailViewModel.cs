6ccf4e9a223aac9daec364102e5b1b6b5ffa5753
tasherul <tasherul@users.noreply.github.com>
2019-03-02 16:43:49 +0600

    update source file
    this is a android sln file to continue

---
+++ stsms/stsms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using stsms.Models;
+
+namespace stsms.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ stsms/stsms/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace stsms.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
